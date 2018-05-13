8f92df4b909e4e75d5d19b9d4ff80fab84ca9df3
A.A <A.A@techacademy.jp>
2018-05-13 07:44:13 +0000

    fix database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180508141426) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "status"
+    t.string   "status2"
   end
 
 end
